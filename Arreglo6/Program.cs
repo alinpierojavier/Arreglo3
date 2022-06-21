c0770533a0ac0e495d9c64204fea8340a1e983dd
alin piero <alinpiero@gmail.com>
2022-06-20 23:45:21 -0500

    Arreglo: Listado de estudiantes y su nota

---
+++ Arreglo6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Arreglo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Empleando arreglos mostrar los nombres de 4 alumnos con sus respectivas notas finales.
+            string[] nombre = new string[5];
+            int[] notas = new int[5];
+
+            for (int i = 0; i < nombre.Length; i++)
+            {
+                Console.WriteLine("Ingrese nombre del estudiante " + i);
+                nombre[i] = Console.ReadLine();
+                Console.WriteLine("Ingresar su nota");
+                notas[i] =Convert.ToInt32(Console.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
